0e9aeaa48ff2b798e797ea3db71d046797467950
Nathan Duddles <nduddles@seas.upenn.edu>
2022-05-11 05:42:03 -0400

    Fix labels in B4 test script

--- scripts/test_B4.sh
+++ scripts/test_B4.sh
@@ -8,13 +8,13 @@ if [ "$#" -ne 1 ]; then
     exit
 fi
 
-if ls $OUTDIR/B2-* 1> /dev/null 2>&1; then
-    echo "$OUTDIR/B2-* exists, please remove it."
+if ls $OUTDIR/B4-* 1> /dev/null 2>&1; then
+    echo "$OUTDIR/B4-* exists, please remove it."
     exit 1
 fi
 
-if ls $OUTDIR/result-B2-compare 1> /dev/null 2>&1; then
-    echo "$OUTDIR/result-B2-compare exists, please remove it."
+if ls $OUTDIR/result-B4-compare 1> /dev/null 2>&1; then
+    echo "$OUTDIR/result-B4-compare exists, please remove it."
     exit 1
 fi
 
@@ -22,19 +22,19 @@ mkdir -p $OUTDIR/result-B4
 
 # Run smartian, ilf, sFuzz, manticore, and mythril.
 for i in $(seq $1); do
-    python $SCRIPTDIR/run_experiment.py B2 smartian 3600
-    # python $SCRIPTDIR/run_experiment.py B2 ilf 3600
-    # python $SCRIPTDIR/run_experiment.py B2 sFuzz 3600
-    # python $SCRIPTDIR/run_experiment.py B2 manticore 3600 B2
-    # python $SCRIPTDIR/run_experiment.py B2 mythril 3600
+    python $SCRIPTDIR/run_experiment.py B4 smartian 600
+    # python $SCRIPTDIR/run_experiment.py B4 ilf 3600
+    # python $SCRIPTDIR/run_experiment.py B4 sFuzz 3600
+    # python $SCRIPTDIR/run_experiment.py B4 manticore 3600 B4
+    # python $SCRIPTDIR/run_experiment.py B4 mythril 3600
 done
 mkdir -p $OUTDIR/result-B4-compare/smartian
 mv $OUTDIR/B4-smartian-* $OUTDIR/result-B4-compare/smartian/
-# mkdir -p $OUTDIR/result-B2-compare/ilf
-# mv $OUTDIR/B2-ilf-* $OUTDIR/result-B2-compare/ilf/
-# mkdir -p $OUTDIR/result-B2-compare/sFuzz
-# mv $OUTDIR/B2-sFuzz-* $OUTDIR/result-B2-compare/sFuzz/
-# mkdir -p $OUTDIR/result-B2-compare/manticore
-# mv $OUTDIR/B2-manticore-* $OUTDIR/result-B2-compare/manticore/
-# mkdir -p $OUTDIR/result-B2-compare/mythril
-# mv $OUTDIR/B2-mythril-* $OUTDIR/result-B2-compare/mythril/
+# mkdir -p $OUTDIR/result-B4-compare/ilf
+# mv $OUTDIR/B4-ilf-* $OUTDIR/result-B4-compare/ilf/
+# mkdir -p $OUTDIR/result-B4-compare/sFuzz
+# mv $OUTDIR/B4-sFuzz-* $OUTDIR/result-B4-compare/sFuzz/
+# mkdir -p $OUTDIR/result-B4-compare/manticore
+# mv $OUTDIR/B4-manticore-* $OUTDIR/result-B4-compare/manticore/
+# mkdir -p $OUTDIR/result-B4-compare/mythril
+# mv $OUTDIR/B4-mythril-* $OUTDIR/result-B4-compare/mythril/
